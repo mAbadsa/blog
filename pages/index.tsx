15968c414bb69fb22b844df906b3c4a1dbef6d66
Muhammad <moh22389@gmail.com>
2021-08-15 15:02:36 +0300

    install material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import type { NextPage } from 'next';
 
 import Layout from '../components/Layout';
 
-import styles from '../styles/Home.module.css'
-
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Layout>
-        <h1>HOMEPAGE</h1>
-        <p>Basic homepage</p>
-      </Layout>
-    </div>
+    <Layout>
+      <h1>HOMEPAGE</h1>
+      <p>Basic homepage</p>
+    </Layout>
   )
 }
 
