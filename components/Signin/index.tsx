2554cc74fbfa07467614aa404649df50b731521b
Muhammad <moh22389@gmail.com>
2021-08-17 16:28:07 +0300

    Create initial component

---
+++ components/Signin/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const Signin: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signin;
---
+++ components/Signin/styles.ts
@@ -0,0 +1,7 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({}),
+);
+
+export default useStyles;
