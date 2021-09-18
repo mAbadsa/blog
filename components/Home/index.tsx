b89a781cc7841104a4867fad5f98395c776256c2
Muhammad <moh22389@gmail.com>
2021-09-18 20:05:23 +0300

    Add Sidebars to Home component

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,27 +1,49 @@
-import React, {FC} from 'react'
-import Link from 'next/link';
+import React, { FC } from "react";
+import Link from "next/link";
 
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
-import { useTheme } from '@material-ui/core'
-import { Link as MUILink } from '@material-ui/core';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import { useTheme } from "@material-ui/core";
+import { Link as MUILink } from "@material-ui/core";
+import SidebarList from "../SidebarList";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const Home: FC = () => {
   const theme = useTheme();
   const classes = useStyles({ theme });
   return (
-    <Grid className={classes.root} container spacing={2} >
-      <Grid item xs={12}>
-        
+    <Container maxWidth={"lg"}>
+      <Grid className={classes.root} container spacing={0}>
+        <Grid
+          className={classes.sidebar}
+          item
+          sm={3}
+          md={3}
+          component={Box}
+          display={{ xs: "none", sm: "block", lg: "block" }}
+        >
+          <SidebarList drawer={false} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <h1>HOMEPAGE</h1>
+          <p>Basic homepage</p>
+        </Grid>
+        <Grid
+          item
+          md={3}
+          component={Box}
+          display={{ xs: "none", sm: "none", md: "block" }}
+        >
+          Right Sidebar
+        </Grid>
       </Grid>
-    </Grid>
-  )
-}
+    </Container>
+  );
+};
 
 export default Home;
--- components/Home/styles.ts
+++ components/Home/styles.ts
@@ -1,10 +1,18 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      display: "flex",
+      justifyContent: "space-around",
     },
-  }),
+    sidebar: {
+      maxWidth: "220px",
+    },
+    // rightSidebar: {
+    //   maxWidth: "240px",
+    // },
+  })
 );
 
 export default useStyles;
