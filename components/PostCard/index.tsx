6b4a3f7e7c6fe7e30fa82a2786dbd182118587e8
Muhammad <moh22389@gmail.com>
2021-10-02 16:42:37 +0300

    Make some change on PostCard style

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { useTheme } from "@material-ui/core";
-import { Link as MUILink } from '@material-ui/core';
+import { Link as MUILink } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -12,6 +12,8 @@ import Avatar from "@material-ui/core/Avatar";
 import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 
+import PostCardAvatar from "./PostCardAvatar";
+
 import useStyles from "./styles";
 
 const PostCard: FC = () => {
@@ -20,44 +22,40 @@ const PostCard: FC = () => {
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia
+        {/* <CardMedia
           component="img"
           alt="Contemplative Reptile"
           height="140"
           image="https://placekitten.com/640/360"
           title="Contemplative Reptile"
-        />
-        <CardContent>
-          <div>
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              R
-            </Avatar>
-          </div>
+        /> */}
+        <PostCardAvatar />
+        <CardContent className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
             <Link href={`/#post-title`} passHref>
-              <MUILink color="textPrimary" >{"Post title"}</MUILink>
+              <MUILink color="textPrimary">{"Post title"}</MUILink>
             </Link>
           </Typography>
           <Typography variant="body2" color="textPrimary" component="p">
             tags
           </Typography>
+          <CardActions className={classes.postCardActions}>
+            <div className={classes.postCard__details}>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+              <Button size="small" color="primary">
+                Learn More
+              </Button>
+            </div>
+            <div className={classes.postCard__save}>
+              <Button size="small" color="primary">
+                Save
+              </Button>
+            </div>
+          </CardActions>
         </CardContent>
       </CardActionArea>
-      <CardActions className={classes.postCardActions}>
-        <div className={classes.postCard__details}>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </div>
-        <div className={classes.postCard__save}>
-          <Button size="small" color="primary">
-            Save
-          </Button>
-        </div>
-      </CardActions>
     </Card>
   );
 };
