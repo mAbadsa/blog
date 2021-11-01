974e9cf5c202cfa054a0b8e6e65b0ff7562f634a
Muhammad <moh22389@gmail.com>
2021-11-01 19:47:34 +0200

    Add marginBottom to PostCard

--- components/PostCard/styles.ts
+++ components/PostCard/styles.ts
@@ -7,6 +7,10 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: '1rem',
       boxShadow: '0 0 0 1px #0909091a !important',
       borderRadius: '0.375rem',
+      marginBottom: theme.spacing(1),
+      '&:last-child': {
+        marginBottom: 0,
+      },
     },
     cardContent: {
       paddingLeft: 'calc(2rem + 0.5rem)',
