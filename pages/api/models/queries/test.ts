3613b415c77b0d1763d2e97560af639213ce54b0
Muhammad <moh22389@gmail.com>
2021-11-13 18:25:43 +0200

    Add sql queries for testing

---
+++ pages/api/models/queries/test.ts
@@ -0,0 +1,18 @@
+import connection from '../connection';
+
+const getUserData = () => {
+  const sql = {
+    text: 'SELECT * FROM users;',
+  };
+  return connection.query(sql);
+};
+
+const addUser = () => {
+  const sql = {
+    text: "INSERT INTO users (username, name, bio, email) VALUES ('mabadsa', 'Muhammad Elabadsa', '...', 'moh1@gmail.com');",
+  };
+  return connection.query(sql);
+};
+
+
+export {getUserData, addUser};
