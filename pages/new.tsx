d7d77f63c0fcb3c4d9e99436793588704c3f46f3
Muhammad <moh22389@gmail.com>
2021-11-21 19:14:55 +0200

    Add ArticleForm to new page for testing

---
+++ pages/new.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import ArticleForm from "../components/ArticleForm";
+
+const New: NextPage = () => {
+  return (
+    <>
+      <ArticleForm />
+    </>
+  );
+};
+
+export default New;
