d8a49ea9d5c3c8b6d4006aae5ae1af77199182a8
mAbadsa <moh22389@gmail.com>
2021-12-08 19:09:52 +0200

    Add Tap and some style

--- components/ArticleForm/styles.ts
+++ components/ArticleForm/styles.ts
@@ -11,6 +11,17 @@ const useStyles = makeStyles((theme: Theme) => ({
     margin: "0 auto",
     fontSize: "1rem",
   },
+  tapPanel: {
+    gridColumnStart: "2",
+    gridColumnEnd: "2",
+    // padding: "2rem 4rem",
+    backgroundColor: "#fff",
+    borderRadius: "0.375rem 0.375rem 0 0",
+    boxShadow: "0 0 0 1px #0909091a",
+    overflowWrap: "anywhere",
+    overflow: "auto",
+    height: "calc(100vh - 56px - 72px)",
+  },
   [theme.breakpoints.between("xs", "sm")]: {
     ArticleForm: {
       gridTemplateColumns: "0 7fr",
