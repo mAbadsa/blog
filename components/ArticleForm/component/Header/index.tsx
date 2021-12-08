c53a9be87cb971fc5fc7054bf21a042095bfb239
mAbadsa <moh22389@gmail.com>
2021-12-08 19:11:36 +0200

    Add FormTabs and Close button to the Header

--- components/ArticleForm/component/Header/index.tsx
+++ components/ArticleForm/component/Header/index.tsx
@@ -3,26 +3,40 @@ import Link from "next/link";
 
 import { useTheme } from "@material-ui/styles";
 import { Link as MUILink } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import Close from "@material-ui/icons/Close";
 import useStyles from "./styles";
 import { Typography } from "@material-ui/core";
 import SVGIcons from "../../../SVG/SVGIcons";
+import FormTabs from "./Tabs";
 
-const Header: FC = () => {
+const Header: FC<{ value: number; handleChange: any }> = ({
+  value,
+  handleChange,
+}) => {
   const theme = useTheme();
   const classes = useStyles({ theme });
 
   return (
     <div className={classes.Header}>
-      <Typography className={classes.logo} component="span">
-        <Link href="/" passHref>
-          <MUILink>
-            <SVGIcons.DEVLogo />
-          </MUILink>
-        </Link>
-      </Typography>
-      <Typography className={classes.title} component="span">
-        Create Post
-      </Typography>
+      <div className={classes.headerTitle}>
+        <Typography className={classes.logo} component="span">
+          <Link href="/" passHref>
+            <MUILink>
+              <SVGIcons.DEVLogo />
+            </MUILink>
+          </Link>
+        </Typography>
+        <Typography className={classes.title} component="span">
+          Create Post
+        </Typography>
+      </div>
+      <div className={classes.headerTaps}>
+        <FormTabs value={value} handleChange={handleChange} />
+        <IconButton className={classes.closeForm}>
+          <Close aria-label="close" />
+        </IconButton>
+      </div>
     </div>
   );
 };
