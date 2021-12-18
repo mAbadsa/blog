23c3ba24b81e105f3eef4fe21b9d63e98470b074
mAbadsa <moh22389@gmail.com>
2021-12-18 12:04:17 +0200

    Fix Tgas url in Tags component

--- components/PostCard/Tags/index.tsx
+++ components/PostCard/Tags/index.tsx
@@ -4,26 +4,22 @@ import { useTheme } from "@material-ui/core";
 import { Link as MUILink } from "@material-ui/core";
 
 import useStyles from "./styles";
-import tagsType from '../../interface/Tags';
+import tagsType from "../../interface/Tags";
 
-const Tags: FC<{tags:tagsType[]}> = ({ tags }) => {
+const Tags: FC<{ tags: tagsType[] }> = ({ tags }) => {
   const theme = useTheme();
   const classes = useStyles({ theme });
-  const tagsElmts: ReactElement[] = tags.map(({id, tag}) => {
+  const tagsElmts: ReactElement[] = tags.map(({ id, tag }) => {
     return (
-      <Link href="" passHref key={id}>
+      <Link href={`/t/${tag}`} passHref key={id}>
         <MUILink>
           <span>#</span>
           <span>{tag}</span>
         </MUILink>
       </Link>
     );
-  })
-  return (
-    <div className={classes.Tags}>
-      {tagsElmts}
-    </div>
-  );
+  });
+  return <div className={classes.Tags}>{tagsElmts}</div>;
 };
 
 export default Tags;
