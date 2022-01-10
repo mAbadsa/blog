05c3ae11288eb5a1b035ea2d054fde0842aa359c
mAbadsa <moh22389@gmail.com>
2022-01-10 11:51:12 +0200

    styling and make it responsive

--- components/Modal/styles.ts
+++ components/Modal/styles.ts
@@ -1,9 +1,21 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {},
-  })
-);
+const useStyles = makeStyles((theme: Theme) => ({
+  Modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "1.5rem",
+  },
+  [theme.breakpoints.between(0, 640)]: {
+    Modal: {
+      maxHeight: "100%",
+      height: "100%",
+      width: "100%",
+      maxWidth: "768px",
+      padding: "0rem",
+    },
+  },
+}));
 
 export default useStyles;
