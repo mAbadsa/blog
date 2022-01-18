fa9198ab53b4605793a7bae50c43eebc9d00aa1c
mAbadsa <moh22389@gmail.com>
2022-01-18 20:45:15 +0200

    handle post new article

--- components/ArticleForm/component/Footer/index.tsx
+++ components/ArticleForm/component/Footer/index.tsx
@@ -1,15 +1,30 @@
-import { FC } from "react";
+import { FC, MouseEventHandler } from "react";
 import { useTheme } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import useStyles from "./styles";
 
-const Footer: FC = () => {
+const Footer: FC<{
+  handlePublishArticle: MouseEventHandler<HTMLButtonElement>;
+  handleSaveDraft: MouseEventHandler<HTMLButtonElement>;
+}> = ({ handlePublishArticle, handleSaveDraft }) => {
   const theme = useTheme();
   const classes = useStyles({ theme });
   return (
     <div className={classes.Footer}>
-      <Button className={classes.publish}>Publish</Button>
-      <Button className={classes.saveDraft}>Save draft</Button>
+      <Button
+        className={classes.publish}
+        tabIndex={1}
+        onClick={handlePublishArticle}
+      >
+        Publish
+      </Button>
+      <Button
+        className={classes.saveDraft}
+        tabIndex={2}
+        onClick={handleSaveDraft}
+      >
+        Save draft
+      </Button>
     </div>
   );
 };
