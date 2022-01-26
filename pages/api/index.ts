4a5b7fe184e366ced43b99599db3ce85bd264595
mAbadsa <moh22389@gmail.com>
2022-01-26 15:22:29 +0200

    prettier

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import {getUserData, addUser} from './models/queries/test';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getUserData, addUser } from "./models/queries/test";
 
 type Data = {
-  name: string,
-  user: any[]
-}
+  name: string;
+  user: any[];
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,5 +13,5 @@ export default async function handler(
 ) {
   await addUser();
   const { rows } = await getUserData();
-  res.status(200).json({ name: 'John Doe', user: rows })
+  res.status(200).json({ name: "John Doe", user: rows });
 }
