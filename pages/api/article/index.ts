36fd82169c561beae20f615eeaf4018f5e21b476
mAbadsa <moh22389@gmail.com>
2022-01-26 15:05:31 +0200

    create add new article api

--- pages/api/article/index.ts
+++ pages/api/article/index.ts
@@ -1,22 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import auth0 from "../../../lib/auth0";
+import { insertNewArticle } from "../models/queries/articles";
+import { getUserByEmail } from "../models/queries/users";
 
 type Data = {
   statusCode?: Number;
   success?: boolean;
-  article: string;
+  article: Object;
 };
 
 export default auth0.withApiAuthRequired(
   async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const data = req.body;
-    console.log(data);
     try {
-      const me = await auth0.getSession(req, res);
-      console.log(me?.user);
-      return res
-        .status(201)
-        .json({ statusCode: 201, success: true, article: data });
+      if (req.method === "POST") {
+        const { data } = req.body;
+        // get the looged user session
+        const me = await auth0.getSession(req, res);
+
+        // check if use is exist in the database
+        const { rows } = await getUserByEmail({ email: me?.user?.email });
+
+        const { rowCount, rows: dbResult } = await insertNewArticle({
+          coverImage: data.coverImage,
+          title: data.title,
+          content: data.textareaValue,
+          slug: "",
+          tags: "data.tags",
+          lastReading: new Date().toISOString(),
+          userId: rows[0].id,
+        });
+
+        if (rowCount < 1) {
+          throw new Error("something went wrong");
+        }
+
+        return res
+          .status(201)
+          .json({ statusCode: 201, success: true, article: dbResult[0] });
+      } else if (req.method === "GET") {
+        return res
+          .status(201)
+          .json({ statusCode: 201, success: true, article: [] });
+      }
     } catch (error) {
       console.log(error);
     }
