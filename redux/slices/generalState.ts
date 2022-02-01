bf0b3471810ff2de393fc26127ac2f688d5d3920
mAbadsa <moh22389@gmail.com>
2022-02-01 18:47:59 +0200

    create general slice

---
+++ redux/slices/generalState.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface GeneralState {}
+
+const initialState: GeneralState = {};
+
+const generalSlice = createSlice({
+  name: "general",
+  initialState,
+  reducers: {},
+});
