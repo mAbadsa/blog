10ba95f67fc5395bcb126f8f9902095c39e02475
mAbadsa <moh22389@gmail.com>
2022-02-03 14:30:58 +0200

    show user profile image

--- components/UserProfile/index.tsx
+++ components/UserProfile/index.tsx
@@ -1,8 +1,32 @@
 import { FC } from "react";
+import { useTheme } from "@material-ui/core";
+import Image from "next/image";
+import useStyles from "./styles";
 
 const UserProfile: FC<{ userData: any }> = ({ userData }) => {
+  const theme = useTheme();
+  const classes = useStyles({ theme });
+
   return (
-    <div>
+    <div className={classes.UserProfile}>
+      <div className={classes.profile}>
+        <div className={classes.layout}>
+          <header className={classes.profileHeader}>
+            <div className={classes.headerTop}>
+              <div className={classes.profileHeaderAvatar}>
+                <Image
+                  className={classes.profileHeaderAvatar_image}
+                  src={userData.data.profile_image}
+                  alt={userData.data.name}
+                  width="120"
+                  height="120"
+                />
+              </div>
+              <div className={classes.headerAction}></div>
+            </div>
+          </header>
+        </div>
+      </div>
       {
         <pre>
           <code>{JSON.stringify(userData.data, null, 4)}</code>
--- components/UserProfile/styles.ts
+++ components/UserProfile/styles.ts
@@ -1,5 +1,53 @@
 import { Theme, makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) => ({}));
+const useStyles = makeStyles((theme: Theme) => ({
+  UserProfile: {
+    scrollMarginTop: "56px",
+    minHeight: "16rem",
+  },
+  profile: {
+    background: "linear-gradient(180deg, #000000 8rem, #00000000 8rem)",
+  },
+  layout: {
+    paddingTop: "4rem",
+    fontSize: "1rem",
+    width: "100%",
+    maxWidth: "1024px",
+    margin: "0 auto",
+    display: "grid",
+    gap: "0.5rem",
+    gridTemplateColumns: "100%",
+    padding: "0.5rem",
+  },
+  headerTop: {
+    position: "relative",
+  },
+  profileHeaderAvatar: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "8rem",
+    height: "8rem",
+    padding: "0.5rem",
+    backgroundColor: "#000000",
+    borderRadius: "100%",
+    verticalAlign: "middle",
+  },
+  profileHeaderAvatar_image: {
+    borderRadius: "100%",
+    width: "100%",
+    height: "100%",
+    display: "inline-block",
+    verticalAlign: "bottom",
+  },
+  headerAction: {},
+  [`@media (max-width: 768px)`]: {
+    layout: {
+      paddingTop: "2rem",
+      padding: "0",
+    },
+  },
+  profileHeader: {},
+}));
 
 export default useStyles;
