3bbfa51e776539138e6a25ac0d2228750f395d2e
mAbadsa <moh22389@gmail.com>
2022-02-22 15:47:35 +0200

    create Button story and make some change on the button component'

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, ReactPropTypes } from "react";
+import { FC, ReactElement, ElementType, ReactNode } from "react";
 import MUIButton from "@material-ui/core/Button";
 import { useTheme } from "@material-ui/core";
 
@@ -8,21 +8,25 @@ const Button: FC<{
   className?: string;
   variant?: "text" | "outlined" | "contained" | undefined;
   color?: "inherit" | "primary" | "secondary" | "default" | undefined;
+  size?: "large" | "medium" | "small";
+  onClick?: (evt: any) => void;
 }> = ({
   className,
   children,
   variant,
   color,
+  size = "medium",
+  onClick,
   ...props
 }): ReactElement<any, any> | null => {
-  const theme = useTheme();
-  const classes = useStyles({ theme });
   return (
     <MUIButton
-      className={`${classes.MUIButton} ${className}`}
+      className={className}
       disableElevation
       variant={variant}
       color={color}
+      size={size}
+      onClick={onClick}
       {...props}
     >
       {children}
