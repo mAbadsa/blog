cbee7c1948c8f3d42499664241896f4b363d40b6
mAbadsa <moh22389@gmail.com>
2022-05-07 14:43:51 +0300

    ...

---
+++ src/components/Promotion/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import PromotionType from "../interface/Promotion";
+import CreateAccount from "./CreateAcount";
+
+const Promotion: PromotionType = {
+  CreateAccount,
+};
+
+export default Promotion;
