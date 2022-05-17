c7437c6d3e39178bbb8d9e36419978c527cb7ac5
mAbadsa <moh22389@gmail.com>
2022-05-17 18:41:43 +0300

    create Footer component

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+import NavLink from '../NavLink';
+
+export const StyledFooter = styled('footer')`
+  ${({ theme }) => `
+    padding: 3rem;
+    background-color: #e5e5e5;
+    color: #404040;
+    text-align: center;
+    ${[theme.breakpoints.down('sm')]} {
+      padding: ${theme.spacing(5)}px;
+    },
+  `}
+`;
+
+export const StyledFooterContainer = styled('div')`
+  ${({ theme }) => `
+    padding: 0.25rem 0;
+    content-visibility: auto;
+    max-width: 1280px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  `}
+`;
+
+export const StyledFooterDescription = styled(Typography)`
+  ${({ theme }) => `
+    font-size: 0.875rem !important;
+  `}
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  ${({ theme }) => `
+    padding: 0 !important;
+  `}
+`;
