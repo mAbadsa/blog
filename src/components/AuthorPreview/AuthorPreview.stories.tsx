3d1bf1bb2c446e2a743ecc6483fb035ca4028b35
mAbadsa <moh22389@gmail.com>
2022-05-17 18:40:18 +0300

    fix component story

--- src/components/AuthorPreview/AuthorPreview.stories.tsx
+++ src/components/AuthorPreview/AuthorPreview.stories.tsx
@@ -1,4 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../styles/theme';
+
 import AuthorPreview from './AuthorPreview';
 
 export default {
@@ -6,6 +9,24 @@ export default {
   component: AuthorPreview,
 } as ComponentMeta<typeof AuthorPreview>;
 
-export const Template: ComponentStory<typeof AuthorPreview> = args => <AuthorPreview {...args} />;
+const Template: ComponentStory<typeof AuthorPreview> = args => (
+  <ThemeProvider theme={theme}>
+    <AuthorPreview {...args} />
+  </ThemeProvider>
+);
 
 export const AuthorPreviewDefault = Template.bind({});
+AuthorPreviewDefault.args = {
+  userdata: {
+    name: 'Muhammad Alabadsa',
+    username: 'mAbadsa',
+    avatar: '',
+    bio: 'Some user info details',
+    email: 'mabadsa@user.com',
+    githubAccount: 'https://github/mAbadsa',
+    websiteUrl: '',
+    location: 'Palestine-Gaza',
+    education: '',
+    joined: '25/3/2021',
+  },
+};
