dc612863fbdcba80a4e6c71cb049c2cbc393f6da
mAbadsa <moh22389@gmail.com>
2022-05-18 15:11:16 +0300

    fix some styles bugs and modifiy display time

--- src/components/PostCard/PostCardAvatar/index.tsx
+++ src/components/PostCard/PostCardAvatar/index.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
-import Link from "next/link";
-import { useTheme, Link as MUILink } from "@material-ui/core";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import useStyles from "./styles";
+import { FC } from 'react';
+import Link from 'next/link';
+import dayjs from 'dayjs';
+import { useTheme, Link as MUILink } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+
+import getTime from '../../../helpers/getTime';
+
+import useStyles from './styles';
 
 const PostCardAvatar: FC<{
   username: string;
@@ -13,17 +17,13 @@ const PostCardAvatar: FC<{
 }> = ({ username, date, slug, avatar }) => {
   const theme = useTheme();
   const classes = useStyles({ theme });
+
   console.log(avatar);
   return (
     <div className={classes.PostCardAvatar}>
       <Link href={`/${username}`} passHref>
         {avatar.length > 0 ? (
-          <Avatar
-            aria-label="user-avatar"
-            className={classes.avatar}
-            alt={username}
-            src={avatar}
-          />
+          <Avatar aria-label="user-avatar" className={classes.avatar} alt={username} src={avatar} />
         ) : (
           <Avatar aria-label="user-avatar" className={classes.avatar}>
             {!avatar && username[0].toUpperCase()}
@@ -34,11 +34,8 @@ const PostCardAvatar: FC<{
         <Button className={classes.username}>{username}</Button>
         <Link href={`/${username}/${slug}`} passHref>
           <MUILink className={classes.avatarMetaPostLink}>
-            <time
-              className={classes.avatarMetaTime}
-              dateTime="$2021-10-1T22:15:00"
-            >
-              {date}
+            <time className={classes.avatarMetaTime} dateTime="$2021-10-1T22:15:00">
+              {dayjs(date).format('MMM DD')} {' ('} {getTime(date)} {' ago) '}
             </time>
           </MUILink>
         </Link>
