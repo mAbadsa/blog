2454b563d27146b1b3aae0e4af25ffb2788d4ca7
mAbadsa <moh22389@gmail.com>
2022-05-20 19:26:59 +0300

    Create AuthenticationModal component

---
+++ src/components/AuthenticationModal/AuthenticationModal.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+import {
+  StyledAuthenticationModal,
+  StyledHeader,
+  StyledCloseButton,
+  StyledImage,
+  StyledModalBody,
+  StyledImageContainer,
+  StyledModalContainer,
+  StyledModalDescription,
+  StyledModalActions,
+  StyledButton,
+} from './styles';
+
+import SVGIcons from '../SVG/SVGIcons';
+
+const LoginModal: FC = () => {
+  return (
+    <StyledAuthenticationModal>
+      <StyledHeader>
+        <Typography variant="h2">Log in to continue</Typography>
+        <StyledCloseButton color="secondary" variant="text">
+          <SVGIcons.Close />
+        </StyledCloseButton>
+      </StyledHeader>
+      <StyledModalBody>
+        <StyledModalContainer>
+          <StyledImageContainer>
+            <StyledImage src="/assets/images/dev_logo.webp" alt="DEV-logo" width="80" height="80" />
+          </StyledImageContainer>
+          <StyledModalDescription>
+            {" We're a place where coders share, stay up-to-date and grow their careers. "}
+          </StyledModalDescription>
+          <StyledModalActions>
+            <StyledButton href="/api/auth/login" variant="contained" color="primary">
+              Log in
+            </StyledButton>
+            <StyledButton href="/api/auth/login" variant="text" color="primary">
+              Create account
+            </StyledButton>
+          </StyledModalActions>
+        </StyledModalContainer>
+      </StyledModalBody>
+    </StyledAuthenticationModal>
+  );
+};
+
+export default LoginModal;
