8ac1ea6937cc47ec226efcd5d63f3d959b70b526
mAbadsa <moh22389@gmail.com>
2022-05-22 14:35:52 +0300

    fix url params

--- src/services/reactions/getReactions.ts
+++ src/services/reactions/getReactions.ts
@@ -5,9 +5,7 @@ const getReactions =
   <T extends { axios: AxiosStatic }>({ axios }: T) =>
   async <U extends { articleId: number }>({ articleId }: U): Promise<AxiosResponse> => {
     console.log(process.env.NEXT_PUBLIC_BASE_UR);
-    const res: AxiosResponse = await axios.get(
-      `http://localhost:3000/api/reactions?article_id=${articleId}`,
-    );
+    const res: AxiosResponse = await axios.get(`http://localhost:3000/api/reactions/${articleId}`);
     return res;
   };
 
