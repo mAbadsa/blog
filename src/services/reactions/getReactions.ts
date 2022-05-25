e79bb94b5436f0eea379f6dccd761d263d538b90
mAbadsa <moh22389@gmail.com>
2022-05-25 10:30:25 +0300

    delete console.log

--- src/services/reactions/getReactions.ts
+++ src/services/reactions/getReactions.ts
@@ -3,7 +3,6 @@ import { AxiosResponse, AxiosStatic } from 'axios';
 const getReactions =
   <T extends { axios: AxiosStatic }>({ axios }: T) =>
   async <U extends { articleId: number }>({ articleId }: U): Promise<AxiosResponse> => {
-    console.log(process.env.NEXT_PUBLIC_BASE_UR);
     const res: AxiosResponse = await axios.get(`http://localhost:3000/api/reactions/${articleId}`);
     return res;
   };
