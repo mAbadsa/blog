873e679497135428db8cb6d5179aa3516f56caa7
mAbadsa <moh22389@gmail.com>
2022-06-02 14:18:44 +0300

    create dashboard main page, user_folloers page and following_tags

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Dashboard from '../../src/screens/Dashboard';
+
+const DashboardPage: NextPage = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
