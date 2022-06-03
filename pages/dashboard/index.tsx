bb4b2cc311f436c5aa1a0272659bc9784735c13c
mAbadsa <moh22389@gmail.com>
2022-06-03 20:01:48 +0300

    protect dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,37 @@
 import type { NextPage } from 'next';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { CircularProgress, Container, Snackbar } from '@material-ui/core';
+
 import Dashboard from '../../src/screens/Dashboard';
 
 const DashboardPage: NextPage = () => {
   return <Dashboard />;
 };
 
-export default DashboardPage;
+export default withPageAuthRequired(DashboardPage, {
+  onRedirecting: () => (
+    <Container
+      maxWidth="sm"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '100vh',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress />
+    </Container>
+  ),
+  onError: error => (
+    <Snackbar
+      open={error && true}
+      autoHideDuration={6000}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+    >
+      <p>{error}</p>
+    </Snackbar>
+  ),
+});
