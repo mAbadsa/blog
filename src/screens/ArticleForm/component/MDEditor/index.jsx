740571ba5db43adbcbf23e51706acf1bb79c31bd
mAbadsa <moh22389@gmail.com>
2022-06-10 06:57:29 +0300

    make textarea focus when add new image

--- src/screens/ArticleForm/component/MDEditor/index.jsx
+++ src/screens/ArticleForm/component/MDEditor/index.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useTheme } from '@material-ui/core';
 import ToolPanel from './ToolPanel';
 import useStyles from './styles';
 
-const MDEditor = ({ handleMDText, mdText }) => {
+const MDEditor = ({ handleMDText, mdText, setCursorPostion, setPanelValue, setRef }) => {
   const theme = useTheme();
   const classes = useStyles({ theme });
+  const textareaRef = useRef();
+
+  function handleFocus(evt) {
+    console.log({ sPostion: evt.target.selectionEnd });
+    setCursorPostion(evt.target.selectionEnd);
+  }
   return (
     <div className={classes.root}>
-      <ToolPanel></ToolPanel>
+      <ToolPanel
+        setPanelValue={setPanelValue}
+        setRef={setRef}
+        textareaRef={textareaRef}
+      ></ToolPanel>
       <div className={classes.MDEditor}>
         <textarea
           className={classes.textArea}
@@ -16,6 +26,9 @@ const MDEditor = ({ handleMDText, mdText }) => {
           onChange={handleMDText}
           placeholder="Write your post content here..."
           value={mdText}
+          onClick={handleFocus}
+          onKeyDown={handleFocus}
+          ref={textareaRef}
         ></textarea>
       </div>
     </div>
