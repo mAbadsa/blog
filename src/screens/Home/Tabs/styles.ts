bc0ccb2e8681c9053e43b1e8254ba9f3ba23bfca
mAbadsa <moh22389@gmail.com>
2022-06-22 14:03:05 +0300

    fix tabs styles

--- src/screens/Home/Tabs/styles.ts
+++ src/screens/Home/Tabs/styles.ts
@@ -8,15 +8,24 @@ const useStyles = makeStyles((theme: Theme) => ({
     backgroundColor: 'transparent !important',
     color: '#090909 !important',
     boxShadow: 'none !important',
+    padding: `${theme.spacing(1)}px 0`,
     '& span[class^=PrivateTabIndicator]': {
       backgroundColor: 'transparent',
     },
     '& button[class^=MuiButtonBase]': {
+      padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
       minWidth: '20px',
       width: 'fit-content',
       textTransform: 'capitalize',
       fontWeight: 'normal',
       fontSize: '1.125rem',
+      transition: 'all 0.25s',
+      '&:hover': {
+        background: '#fff',
+        color: '#3b4ce5',
+        borderRadius: '0.375rem',
+        transition: 'all 0.25s',
+      },
     },
     '& button[aria-selected=true]': {
       fontWeight: 'bold',
