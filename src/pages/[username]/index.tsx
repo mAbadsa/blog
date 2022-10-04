f540a06b43c33569927a7f3ab0516c0381ac4b0f
mAbadsa <moh22389@gmail.com>
2022-10-04 10:49:07 +0300

    fix some path error

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -93,7 +93,7 @@ export const getServerSideProps: GetServerSideProps<
         data: {
           user: null,
           error: !error.response.data.success,
-          message: error.response.data.error,
+          message: error.response.data.error || null,
         },
       },
     };
