4c1c96d30f236f95e74bd12617f0cfe2756587e9
mAbadsa <moh22389@gmail.com>
2022-10-15 13:42:42 +0300

    add delete reading_list item api controllers logic

--- src/pages/api/reading-list/index.ts
+++ src/pages/api/reading-list/index.ts
@@ -4,6 +4,7 @@ import connection from '@pages/api/models/connection';
 import {
   insertArticleToReadingList,
   getUserReadingList,
+  deleteArticleReadlinList,
 } from '@pages/api/models/queries/reading-list';
 
 import { getUserByEmail } from '@pages/api/models/queries/users';
@@ -36,6 +37,31 @@ export default auth0.withApiAuthRequired(
           throw new Error('something went wrong');
         }
         return res.status(201).json({ success: true });
+      } else if (req.method === 'DELETE') {
+        const { id } = req.body;
+        const { rowCount: resultCount, rows: user } = await getUserByEmail({
+          email: me?.user?.email,
+        });
+
+        if (resultCount < 1) {
+          throw new Error('something went wrong');
+        }
+
+        const { rowCount: deleteCount } = await deleteArticleReadlinList({ connection })({ id });
+
+        if (deleteCount < 1) {
+          throw new Error('something went wrong');
+        }
+
+        const { rowCount, rows } = await getUserReadingList({ connection })({
+          user_id: user[0].id,
+        });
+
+        if (rowCount < 1) {
+          throw new Error('something went wrong');
+        }
+
+        return res.status(200).json({ success: true, articles: rows });
       } else {
         const { rowCount: resultCount, rows: user } = await getUserByEmail({
           email: me?.user?.email,
