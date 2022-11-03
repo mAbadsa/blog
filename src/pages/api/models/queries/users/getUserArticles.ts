9337d650e6769447c00c1c6198aa7a0cba3db41d
mAbadsa <moh22389@gmail.com>
2022-11-03 14:53:11 +0200

    fix getuserArticle query

--- src/pages/api/models/queries/users/getUserArticles.ts
+++ src/pages/api/models/queries/users/getUserArticles.ts
@@ -8,7 +8,7 @@ const getUserArticles =
   <U extends { connection: Pool }>({ connection }: U) =>
   <T extends UserType>({ username }: T): Promise<QueryResult<any>> => {
     const sql: QueryConfig<any[]> = {
-      text: `SELECT a.id, a.title, a.slug, a.temp_slug,a.status, a.temp_preview_link FROM articles AS a INNER JOIN users AS u ON u.id = a.user_id WHERE u.username = $1;`,
+      text: `SELECT a.id, a.title, a.slug, a.temp_slug,a.status FROM articles AS a INNER JOIN users AS u ON u.id = a.user_id WHERE u.username = $1;`,
       values: [username],
     };
     return connection.query(sql);
