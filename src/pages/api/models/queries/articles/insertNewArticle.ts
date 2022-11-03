3649dc0495f29a7a2a2d0134564a6e93dbac67e9
mAbadsa <moh22389@gmail.com>
2022-11-03 14:52:16 +0200

    fix insertNewArticle query

--- src/pages/api/models/queries/articles/insertNewArticle.ts
+++ src/pages/api/models/queries/articles/insertNewArticle.ts
@@ -3,9 +3,11 @@ import { QueryConfig, QueryResult, Pool } from 'pg';
 type articleObj = {
   title: string;
   slug: string;
+  tempSlug: string;
   content: string;
   coverImage: string;
   tags: string;
+  status: string;
   lastReading: string;
   userId: string;
 };
@@ -15,15 +17,17 @@ const insertNewArticle =
   <T extends articleObj>({
     title,
     slug,
+    tempSlug,
     content,
     coverImage,
     tags,
+    status,
     lastReading,
     userId,
   }: T): Promise<QueryResult<any>> => {
     const sql: QueryConfig<any[]> = {
-      text: `INSERT INTO articles(title, slug, content, cover_image, tags, last_reading, user_id) VALUES($1, $2, $3, $4, $5, $6, $7);`,
-      values: [title, slug, content, coverImage, tags, lastReading, userId],
+      text: `INSERT INTO articles(title, slug, temp_slug, content, cover_image, tags, status, last_reading, user_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
+      values: [title, slug, tempSlug, content, coverImage, tags, status, lastReading, userId],
     };
 
     return connection.query(sql);
