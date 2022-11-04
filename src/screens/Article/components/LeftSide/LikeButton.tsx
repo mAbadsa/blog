865f09befac3f641f46f1874fcc0964a974fef3f
mAbadsa <moh22389@gmail.com>
2022-11-04 18:07:11 +0200

    create LikeButton to move like reaction logic to it

---
+++ src/screens/Article/components/LeftSide/LikeButton.tsx
@@ -0,0 +1,49 @@
+import { FC, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { usePostReactionMutation } from '@redux/slices/api';
+import { RootState } from '@redux/store';
+import { StyledButton, StyledIconContainer, StyledReactCount } from './styles';
+import SVGIcons from '@components/SVG/SVGIcons';
+
+const LikeButton: FC<{ articleId: number; likesNumber: number; isLiked: Boolean }> = ({
+  articleId,
+  likesNumber,
+  isLiked,
+}) => {
+  const state = useSelector((state: RootState) => state.auth) as any;
+  const [isLikedState, setIsLiked] = useState<Boolean>(isLiked);
+  const [likesCount, setLikesCount] = useState<number>(likesNumber);
+  const [postReaction, { data, isLoading, error }] = usePostReactionMutation();
+  const handleReaction = async () => {
+    try {
+      const res = (await postReaction({
+        reactableId: articleId,
+        category: 'Like',
+        reactableType: 'Article',
+      }).unwrap()) as any;
+
+      if (!isLoading && !error) {
+        setLikesCount(res?.likes.length);
+        setIsLiked(
+          res.likes.some(
+            (like: { user_id: Number | undefined }) =>
+              like.user_id === state.user_id && state.isAuth === true,
+          ),
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <StyledButton variant="text" color="secondary" onClick={handleReaction} isliked={isLikedState}>
+      <StyledIconContainer className="like-container">
+        {isLikedState ? <SVGIcons.FilledLike /> : <SVGIcons.Like />}
+      </StyledIconContainer>
+      <StyledReactCount className="reaction-count_like">{likesCount}</StyledReactCount>
+    </StyledButton>
+  );
+};
+
+export default LikeButton;
