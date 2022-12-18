0229ae10171a1df2846363bf79f193498b71620d
mAbadsa <moh22389@gmail.com>
2022-12-18 09:46:08 +0200

    refactor redux api

---
+++ src/redux/apis/readingListApi.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery, Api } from '@reduxjs/toolkit/query/react';
+import type { ReactionPayload } from '../slices/type';
+
+const readingListApi = createApi({
+  reducerPath: 'readingListApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BASE_URL }),
+  tagTypes: ['ReadingList'],
+  endpoints: builder => ({
+    getReadingList: builder.query({
+      query: () => 'api/reading-list',
+      providesTags: ['ReadingList'],
+    }),
+    postReaction: builder.mutation<any, Partial<ReactionPayload>>({
+      invalidatesTags: ['ReadingList'],
+      query: ({ reactableId, category, reactableType }) => ({
+        url: `api/reactions`,
+        method: 'POST',
+        body: {
+          reactable_id: reactableId,
+          category,
+          reactable_type: reactableType,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { usePostReactionMutation, useGetReadingListQuery } = readingListApi;
+export { readingListApi };
---
+++ src/redux/index.ts
@@ -0,0 +1,60 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, PersistConfig, persistStore } from 'redux-persist';
+
+import { articleApi } from '@redux/apis/articlesApi';
+import { readingListApi } from '@redux/apis/readingListApi';
+import { userApi } from '@redux/apis/userApi';
+import authReducer from '@redux/slices/authSlice';
+import readingListSlice from '@redux/slices/readingList';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  readingList: readingListSlice,
+  auth: authReducer,
+  [articleApi.reducerPath]: articleApi.reducer,
+  [readingListApi.reducerPath]: readingListApi.reducer,
+  [userApi.reducerPath]: userApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware()
+      .concat(articleApi.middleware)
+      .concat(readingListApi.middleware)
+      .concat(userApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const persistor = persistStore(store);
+
+export {} from './slices/articleSlice';
+export {} from './slices/authSlice';
+export {} from './slices/readingList';
+export {
+  useGetArtcilesQuery,
+  usePostArticleMutation,
+  useUpdateArticleMutation,
+  usePostDraftArticleMutation,
+} from './apis/articlesApi';
+export { useGetReadingListQuery, usePostReactionMutation } from './apis/readingListApi';
+export { useGetUserProfileQuery } from './apis/userApi';
