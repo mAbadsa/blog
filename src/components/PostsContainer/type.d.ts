69f060174dd7c3e7ddb2f1cfb19ed1f785c7b8ad
mAbadsa <moh22389@gmail.com>
2022-12-19 06:24:20 +0200

    move dbData type to separate file

---
+++ src/components/PostsContainer/type.d.ts
@@ -0,0 +1,23 @@
+export interface dbData {
+  id: number;
+  title: string;
+  slug: string;
+  content: string;
+  cover_image: string;
+  tags: string;
+  last_reading: number;
+  created_at: string;
+  updated_at: string;
+  user_id: number;
+  username: string;
+  name: string;
+  email: string;
+  bio: string;
+  profile_image: string;
+  location: string;
+  github_account: string;
+  website_url: string;
+  user_created_at: string;
+  likeId: number | null;
+  commentId: number | null;
+}
